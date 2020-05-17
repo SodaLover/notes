ca4ab42ec67003053d6c071a828e6a6900503d99
SodaLover <32605003+SodaLover@users.noreply.github.com>
2020-05-16 21:27:36 -0400

    Add files via upload

---
+++ notes.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+using namespace std;
+
+int main () {
+    time_t now = time(0);
+    char* dt = ctime(&now);
+    ofstream myfile;
+    std::cout << ": ";
+    string x{};
+    std::cin >> x;
+    myfile.open ("/home/jingle/notes.txt", ios_base::app);
+    myfile << "---- " << dt;
+    myfile << x << '\n';
+    myfile << "\n";
+    myfile.close();
+    return 0;
+}
